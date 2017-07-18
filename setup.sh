e0b99602ba248cd28b8c31e9407484f5394fd851
bello12 <belloni.org@gmail.com>
2017-07-18 16:29:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,17 +3,17 @@
 sudo apt-get update
 #LAMP
 sudo apt-get install apache2
-sudo apt-get install mysql-server 
+sudo apt-get install mysql-server
 sudo apt-get install libapache2-mod-auth-mysql 
 sudo apt-get install php7.0-mysql
 sudo apt-get install php7.0 
 sudo apt-get install libapache2-mod-php7.0 
-sudo apt-get install php7.0-mcrypt 
-sudo apt-get install php7.0-zip 
-sudo apt-get install php7.0-xml 
-sudo apt-get install php7.0-mbstring
+sudo apt-get install php7.0-mcrypt
 #/LAMP
 sudo apt-get upgrade
+sudo apt-get install php7.0-zip 
+sudo apt-get install php7.0-xml
+sudo apt-get install php7.0-mbstring
 sudo apt-get install composer
 sudo composer global require "laravel/installer" 
 sudo service apache2 restart
@@ -22,5 +22,15 @@ read ProjectName
 sudo ./.composer/vendor/bin/laravel new $ProjectName
 sudo mv $ProjectName .composer/vendor
 sudo mv .composer/vendor ../../var/www/html
-echo "Change syntax at line 12 in /etc/apache2/sites-available/000-default.conf"
-echo "/var/www/html -----> /var/www/html/vendor/$ProjectName/public"
+
+cd ../../etc/apache2/sites-available/
+sudo touch temp.conf
+sudo chmod 777 temp.conf
+sudo chmod 777 000-default.conf
+sudo sed 's%DocumentRoot /var/www/html%DocumentRoot /var/www/html/vendor/'$ProjectName'/public%g'  000-default.conf > temp.conf
+
+#rimuovo il file originale, e poi con mv cambio il nome del file
+sudo rm 000-default.conf
+sudo mv temp.conf 000-default.conf
+
+sudo service apache2 restart
