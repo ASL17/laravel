e4e1af27c1edb9b3869f71e7a4d23c62c464cbd6
bello12 <belloni.org@gmail.com>
2017-07-18 10:12:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,6 @@ configurazione php.ini
 sudo apt-get install composer
 sudo composer global require "laravel/installer" 
 sudo service apache2 restart
-cd /var/www/html
 echo "Type the project name: followed by [ENTER]"
 read ProjectName
 sudo ./.composer/vendor/bin/laravel new $ProjectName
