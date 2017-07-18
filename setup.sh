2ce320361d6f4436187f395d3e9982f299b5eeb8
bello12 <belloni.org@gmail.com>
2017-07-18 11:02:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 #LAMP
 sudo apt-get install apache2
 sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install php7.0 libapache2-mod-php7.0 php7.0-mcrypt
 #/LAMP
 sudo apt-get upgrade
 sudo apt-get install php7.0-zip php7.0-xml
