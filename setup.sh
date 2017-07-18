790019adf2ae0464baeabff9b7598a4fa704e07a
bello12 <belloni.org@gmail.com>
2017-07-18 15:43:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,17 @@
 sudo apt-get update
 #LAMP
 sudo apt-get install apache2
-sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
-sudo apt-get install php7.0 libapache2-mod-php7.0 php7.0-mcrypt
+sudo apt-get install mysql-server 
+sudo apt-get install libapache2-mod-auth-mysql 
+sudo apt-get install php7.0-mysql
+sudo apt-get install php7.0 
+sudo apt-get install libapache2-mod-php7.0 
+sudo apt-get install php7.0-mcrypt 
+sudo apt-get install php7.0-zip 
+sudo apt-get install php7.0-xml 
+sudo apt-get install php7.0-mbstring
 #/LAMP
 sudo apt-get upgrade
-sudo apt-get install php7.0-zip php7.0-xml php7.0-mbstring
 sudo apt-get install composer
 sudo composer global require "laravel/installer" 
 sudo service apache2 restart
