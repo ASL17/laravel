d6ab7e74e2783021936219b6fdf293afa5eda003
2000andrea <andrea.acito.2000@gmail.com>
2017-07-20 12:35:02 +0200

    Update ProjectCreator.sh
    -Made sed safer
    -Added chmod 777 to prevent access denied errors from apache

--- ProjectCreator.sh
+++ ProjectCreator.sh
@@ -7,12 +7,15 @@ read ProjectName
 sudo ./../../var/www/html/vendor/bin/laravel new $ProjectName
 sudo mv $ProjectName ../../var/www/html/vendor
 
+sudo chmod 777 ../../var/www/html/vendor/$ProjectName/storage/views
+sudo chmod 777 ../../var/www/html/vendor/$ProjectName/storage/logs
+
 cd ../../etc/apache2/sites-available/
 sudo touch temp.conf
 sudo chmod 777 temp.conf
 sudo chmod 777 000-default.conf
 
-sudo sed 's%DocumentRoot /var/www/html%DocumentRoot /var/www/html/vendor/'$ProjectName'/public%g'  000-default.conf > temp.conf
+sudo sed 's%^DocumentRoot .*%DocumentRoot /var/www/html/vendor/'$ProjectName'/public%g'  000-default.conf > temp.conf
 
 #rimuovo il file originale, e poi con mv cambio il nome del file
 sudo rm 000-default.conf
