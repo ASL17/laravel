b64aa5efffe0dbb1a1307fe9e21b424e726d4b6a
2000andrea <andrea.acito.2000@gmail.com>
2017-07-21 11:26:28 +0200

    Update RandomPasswordPageSetup.sh

--- RandomPasswordPage/RandomPasswordPageSetup.sh
+++ RandomPasswordPage/RandomPasswordPageSetup.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+cd routes/
+
+sudo wget http://raw.githubusercontent.com/ASL17/laravel/master/RandomPasswordPage/web.php
+
+sudo rm web.php
+sudo mv web.php.1 web.php
+
+cd ../resources/views/
+
+#remove possibly old-version of a previous page
+if [ -e generatore_passwd.blade.php ]
+then
+        sudo rm generatore_passwd.blade.php
+fi
+
+sudo wget https://raw.githubusercontent.com/ASL17/laravel/master/RandomPasswordPage/generatore_passwd.blade.php
+
+
+
